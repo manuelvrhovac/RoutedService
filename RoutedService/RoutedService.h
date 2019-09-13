8f19d40373ee92b21fe39d0302f2304dc8988810
manuelvrhovac <manuel.vrhovac@icloud.com>
2019-09-13 17:52:49 +0200

    Initial Commit

---
+++ RoutedService/RoutedService.h
@@ -0,0 +1,16 @@
+//
+// Created by Manuel Vrhovac on 13/09/2019.
+// Copyright © 2019 Manuel Vrhovac. All rights reserved.
+// 
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RoutedService.
+FOUNDATION_EXPORT double RoutedServiceVersionNumber;
+
+//! Project version string for RoutedService.
+FOUNDATION_EXPORT const unsigned char RoutedServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoutedService/PublicHeader.h>
+
+
